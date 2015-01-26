6f7b70f0aabc114839fbe7e7fd33da3d4eff8727
wuzhongju <wuzhongju@126.com>
2015-01-26 13:47:24 +0800

    update

--- src/main/java/com/elex/ssp/mlctr/idx/PrepareForIndex.java
+++ src/main/java/com/elex/ssp/mlctr/idx/PrepareForIndex.java
@@ -30,7 +30,7 @@ public class PrepareForIndex {
 
 		Connection con = HiveOperator.getHiveConnection();
 		Statement stmt = con.createStatement();
-		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.nation.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
+		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.user.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
 			    " select distinct concat_ws('_',ft,fv) from feature_merge where ft ='user'";
 		stmt.execute(hql);
 		stmt.close();
@@ -44,6 +44,7 @@ public class PrepareForIndex {
 		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.word.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
 				    " select distinct concat_ws('_',source,word) from tfidf";
 		stmt.execute(hql);
+		System.out.println(hql);
 		stmt.close();
 		
 	}
@@ -55,6 +56,7 @@ public class PrepareForIndex {
 		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.other.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
 				    " select distinct concat_ws('_',ft,fv) from feature_merge where ft !='keyword' and ft !='user' and ft not like 'query%'";
 		stmt.execute(hql);
+		System.out.println(hql);
 		stmt.close();
 		
 	}
@@ -66,6 +68,7 @@ public class PrepareForIndex {
 		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.nation.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
 				" select distinct concat_ws('_','nation',nation) from log_merge where day='"+Constants.getLastFiveDay()+"'";
 		stmt.execute(hql);
+		System.out.println(hql);
 		stmt.close();
 	}
 
@@ -76,6 +79,7 @@ public class PrepareForIndex {
 		String hql ="INSERT OVERWRITE LOCAL DIRECTORY '"+IdxType.os.getSrc()+"' ROW format delimited FIELDS TERMINATED BY ',' stored AS textfile" +
 				" select distinct concat_ws('_','os',os) from log_merge where day='"+Constants.getLastFiveDay()+"'";
 		stmt.execute(hql);
+		System.out.println(hql);
 		stmt.close();
 	}
 }
