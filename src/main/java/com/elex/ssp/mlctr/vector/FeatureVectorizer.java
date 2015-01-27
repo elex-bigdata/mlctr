ea96beb6779e97d40790f52ce5b02e7889355033
wuzhongju <wuzhongju@126.com>
2015-01-27 18:12:46 +0800

    update

--- src/main/java/com/elex/ssp/mlctr/vector/FeatureVectorizer.java
+++ src/main/java/com/elex/ssp/mlctr/vector/FeatureVectorizer.java
@@ -127,7 +127,7 @@ public class FeatureVectorizer extends Configured implements Tool {
 	public static class MyMapper extends Mapper<LongWritable, Text, Text, Text> {
 		
 		private String[] values;
-		private String newKey,newVal,t;
+		private String newKey,newVal;
 		
 		@Override
 		protected void map(LongWritable key, Text value, Context context)
@@ -137,24 +137,25 @@ public class FeatureVectorizer extends Configured implements Tool {
 			//uid,pid,ip,time,nation,ua,os,adid,ref,opt,impr,click
 			
 			try {
-				String[] tF = TimeUtils.getTimeDimension(new String[]{values[3],""});
-				for(String td:tF){
-					t=t+ FeaturePrefix.time.getsName()+"_"+td+"/t";
+				String[] tF = TimeUtils.getTimeDimension(new String[]{values[3],values[4]});
+				if(tF.length == 3){
+					newKey = FeaturePrefix.user.getsName()+"_"+values[0]+"/t"
+							   + FeaturePrefix.project.getsName()+"_"+values[1]+"/t"
+							   + FeaturePrefix.area.getsName()+"_"+Constants.getArea(values[2])+"/t"
+							   + FeaturePrefix.time.getsName()+"_"+tF[0]+"/t"
+							   + FeaturePrefix.time.getsName()+"_"+tF[1]+"/t"
+							   + FeaturePrefix.time.getsName()+"_"+tF[2]+"/t"
+							   + FeaturePrefix.nation.getsName()+"_"+values[4]+"/t"
+							   + FeaturePrefix.browser.getsName()+"_"+values[5]+"/t"
+							   + FeaturePrefix.os.getsName()+"_"+values[6]+"/t"
+							   + FeaturePrefix.adid.getsName()+"_"+values[7]+"/t"
+							   + FeaturePrefix.ref.getsName()+"_"+values[8]+"/t"
+							   + FeaturePrefix.opt.getsName()+"_"+values[9];
+						newVal = values[10]==null?"0":values[10]+"/t"+values[11]==null?"0":values[11];
+						
+						context.write(new Text(newKey), new Text(newVal));
 				}
 				
-				newKey = FeaturePrefix.user.getsName()+"_"+values[0]+"/t"
-						   + FeaturePrefix.project.getsName()+"_"+values[1]+"/t"
-						   + FeaturePrefix.area.getsName()+"_"+Constants.getArea(values[2])+"/t"
-						   + t
-						   + FeaturePrefix.nation.getsName()+"_"+values[4]+"/t"
-						   + FeaturePrefix.browser.getsName()+"_"+values[5]+"/t"
-						   + FeaturePrefix.os.getsName()+"_"+values[6]+"/t"
-						   + FeaturePrefix.adid.getsName()+"_"+values[7]+"/t"
-						   + FeaturePrefix.ref.getsName()+"_"+values[8]+"/t"
-						   + FeaturePrefix.opt.getsName()+"_"+values[9];
-					newVal = values[10]==null?"0":values[10]+"/t"+values[11]==null?"0":values[11];
-					
-					context.write(new Text(newKey), new Text(newVal));
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
