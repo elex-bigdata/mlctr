3d38283d9c047e02c9a63250240a1ba69e7c476b
wuzhongju <wuzhongju@126.com>
2015-02-11 09:57:12 +0800

    scheduler

--- src/main/java/com/elex/ssp/mlctr/Scheduler.java
+++ src/main/java/com/elex/ssp/mlctr/Scheduler.java
@@ -4,8 +4,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.elex.ssp.mlctr.idx.FeatureValueEncoder;
+import com.elex.ssp.mlctr.idx.IndexLoader;
+import com.elex.ssp.mlctr.idx.PrepareForIndex;
+import com.elex.ssp.mlctr.vector.FeatureVectorizer;
+import com.elex.ssp.mlctr.vector.UserWordFeature;
 
 
 public class Scheduler {
@@ -25,17 +32,76 @@ public class Scheduler {
 		
 		// stage 0
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			log.info("create tables!!!");
-			//success = createTables();
-			if (success != 0) {
-				log.error("create tables ERROR!!!,SYSTEM EXIT!!!");
+			log.info("prepare feature value for index encoder!!!");
+			try{
+				PrepareForIndex.prepareAll();
+			}catch(Exception se){
+				log.error("prepare feature value for index encoder ERROR!!!,SYSTEM EXIT!!!");
 				System.exit(success);
-			}
-			log.info("create tables SUCCESS!!!");
+			}			
+			log.info("prepare feature value for index encoder SUCCESS!!!");
 		}
 		
+		//stage 1
+		if (shouldRunNextPhase(stageArgs, currentPhase)) {
+			log.info("feature value encode START!!!");
+			try{
+				FeatureValueEncoder.processAll();
+			}catch(Exception se){
+				log.error("feature value encode ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
+			}		
+			log.info("feature value encode SUCCESS!!!");
+		}
 		
+		//stage 2
+		if (shouldRunNextPhase(stageArgs, currentPhase)) {
+			log.info("load index to hive and hbase START!!!");
+			try{
+				IndexLoader.load();
+			}catch(Exception se){
+				log.error("load index to hive and hbaseERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
+			}		
+			log.info("load index to hive and hbase SUCCESS!!!");
+		}
 		
+		//stage 3
+		if (shouldRunNextPhase(stageArgs, currentPhase)) {
+			log.info("create user word vector and load to hbase START!!!");
+			try{
+				UserWordFeature.createUserWordFeatureFile();
+				UserWordFeature.loadUserWordVectorToHbase();
+			}catch(Exception se){
+				log.error("create user word vector and load to hbase ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
+			}		
+			log.info("load index to hive and hbase SUCCESS!!!");
+		}
+		
+		//stage 4
+		if (shouldRunNextPhase(stageArgs, currentPhase)) {
+			log.info("prepare training data START!!!");
+			try{
+				ToolRunner.run(new Configuration(), new FeatureVectorizer(), new String[]{"train","noskip","20"});
+			}catch(Exception se){
+				log.error("prepare training data ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
+			}		
+			log.info("prepare training data SUCCESS!!!");
+		}
+		
+		//stage 5
+		if (shouldRunNextPhase(stageArgs, currentPhase)) {
+			log.info("prepare test data START!!!");
+			try{
+				ToolRunner.run(new Configuration(), new FeatureVectorizer(), new String[]{"test","noskip","20"});
+			}catch(Exception se){
+				log.error("prepare test data ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
+			}		
+			log.info("prepare test data SUCCESS!!!");
+		}
 		
 		HiveOperator.closeConn();
 	}
