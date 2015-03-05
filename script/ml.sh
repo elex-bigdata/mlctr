15b62b58a395344e3fb1fe08876338c3f74ef697
wuzhongju <wuzhongju@126.com>
2015-03-05 10:22:28 +0800

    update shell jar name

--- script/ml.sh
+++ script/ml.sh
@@ -5,7 +5,7 @@ cluster
 jardir='/home/hadoop/git_project_home/mlctr/target/'
 workdir='/home/hadoop/wuzhongju/ctr/'
 datadir='/data1/ssp/mlctr/'
-hadoop jar $jardir'mlctr-0.0.1-SNAPSHOT.jar' com.elex.ssp.mlctr.Scheduler 0 5 >>$workdir'train.log' 2>&1
+hadoop jar $jardir'mlctr-2.0.jar' com.elex.ssp.mlctr.Scheduler 0 5 >>$workdir'train.log' 2>&1
 rm -rf $datadir'*.txt'
 hadoop fs -getmerge /ssp/mlctr/test/output/part-r-* $datadir'test_id.txt'
 hadoop fs -getmerge /ssp/mlctr/train/output/part-r-* $datadir'train_id.txt'
@@ -20,7 +20,7 @@ echo $size
 if [ $size -gt 10 ];then
    ratio=`echo "scale=2;10/$size" | bc`
    echo $ratio
-   java -cp .:$jardir'mlctr-0.0.1-SNAPSHOT.jar' com.elex.ssp.mlctr.liblinear.GJFormatSample $ratio $datadir'train_id.txt' $datadir'train_sam.txt'
+   java -cp .:$jardir'mlctr-2.0.jar' com.elex.ssp.mlctr.liblinear.GJFormatSample $ratio $datadir'train_id.txt' $datadir'train_sam.txt'
    $workdir'liblinear_modified/'train -s 6 $datadir'train_sam.txt' $workdir'model/ctr.model'
    $workdir'liblinear_modified/'predict -b 1 $datadir'test_id.txt' $workdir'model/ctr.model' $workdir'output/ctr.out'
 else
