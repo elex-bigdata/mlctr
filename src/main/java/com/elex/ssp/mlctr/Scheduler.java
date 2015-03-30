399d23af089d6ff685a1f4ec5484e147c1c6956c
wuzhongju <wuzhongju@126.com>
2015-03-30 09:39:56 +0800

    log

--- src/main/java/com/elex/ssp/mlctr/Scheduler.java
+++ src/main/java/com/elex/ssp/mlctr/Scheduler.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.elex.ssp.mlctr.idx.FeatureValueEncoder;
 import com.elex.ssp.mlctr.idx.IndexLoader;
@@ -15,6 +17,8 @@ import com.elex.ssp.mlctr.vector.UserWordFeature;
 
 public class Scheduler {
 	
+	private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
+
 	/**
 	 * @param args
 	 * @throws Exception 
@@ -25,79 +29,78 @@ public class Scheduler {
 		AtomicInteger currentPhase = new AtomicInteger();
 		String[] stageArgs = { otherArgs[0], otherArgs[1] };// 运行阶段控制参数
 		int success = 0;
-		int error = 1;
 		
 		// stage 0
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("prepare feature value for index encoder!!!");
+			log.info("prepare feature value for index encoder!!!");
 			try{
 				PrepareForIndex.prepareAll();
 			}catch(Exception se){
-				System.out.println("prepare feature value for index encoder ERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("prepare feature value for index encoder ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}			
-			System.out.println("prepare feature value for index encoder SUCCESS!!!");
+			log.info("prepare feature value for index encoder SUCCESS!!!");
 		}
 		
 		//stage 1
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("feature value encode START!!!");
+			log.info("feature value encode START!!!");
 			try{
 				FeatureValueEncoder.processAll();
 			}catch(Exception se){
-				System.out.println("feature value encode ERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("feature value encode ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}		
-			System.out.println("feature value encode SUCCESS!!!");
+			log.info("feature value encode SUCCESS!!!");
 		}
 		
 		//stage 2
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("load index to hive and hbase START!!!");
+			log.info("load index to hive and hbase START!!!");
 			try{
 				IndexLoader.load();
 			}catch(Exception se){
-				System.out.println("load index to hive and hbaseERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("load index to hive and hbaseERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}		
-			System.out.println("load index to hive and hbase SUCCESS!!!");
+			log.info("load index to hive and hbase SUCCESS!!!");
 		}
 		
 		//stage 3
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("create user word vector and load to hbase START!!!");
+			log.info("create user word vector and load to hbase START!!!");
 			try{
 				UserWordFeature.createUserWordFeatureFile();
 				UserWordFeature.loadUserWordVectorToHbase();
 			}catch(Exception se){
-				System.out.println("create user word vector and load to hbase ERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("create user word vector and load to hbase ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}		
-			System.out.println("create user word vector and load to hbase SUCCESS!!!");
+			log.info("create user word vector and load to hbase SUCCESS!!!");
 		}
 		
 		//stage 4
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("prepare training data START!!!");
+			log.info("prepare training data START!!!");
 			try{
 				ToolRunner.run(new Configuration(), new FeatureVectorizer(), new String[]{"train","noskip","20"});
 			}catch(Exception se){
-				System.out.println("prepare training data ERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("prepare training data ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}		
-			System.out.println("prepare training data SUCCESS!!!");
+			log.info("prepare training data SUCCESS!!!");
 		}
 		
 		//stage 5
 		if (shouldRunNextPhase(stageArgs, currentPhase)) {
-			System.out.println("prepare test data START!!!");
+			log.info("prepare test data START!!!");
 			try{
 				ToolRunner.run(new Configuration(), new FeatureVectorizer(), new String[]{"test","noskip","20"});
 			}catch(Exception se){
-				System.out.println("prepare test data ERROR!!!,SYSTEM EXIT!!!");
-				System.exit(error);
+				log.error("prepare test data ERROR!!!,SYSTEM EXIT!!!");
+				System.exit(success);
 			}		
-			System.out.println("prepare test data SUCCESS!!!");
+			log.info("prepare test data SUCCESS!!!");
 		}
 		
 		HiveOperator.closeConn();
@@ -111,7 +114,7 @@ public class Scheduler {
 	    boolean phaseSkipped = (startPhase != null && phase < Integer.parseInt(startPhase))
 	        || (endPhase != null && phase > Integer.parseInt(endPhase));
 	    if (phaseSkipped) {
-	    	System.out.println("Skipping phase {}"+phase);
+	      log.info("Skipping phase {}", phase);
 	    }
 	    return !phaseSkipped;
 	  }
